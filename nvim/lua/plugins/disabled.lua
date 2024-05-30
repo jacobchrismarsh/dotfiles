e0f65ce2e13246c3fbf2454ea60672429edd8e18
Jacob Marshall <jacobmarshall@Jacobs-Laptop.local>
2024-05-29 23:39:42 -0700

    disable the tabs in nvim

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,8 +3,7 @@ return {
   { "folke/todo-comments", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
-  { "nvim-neo-tree/neo-tree", enabled = false },
-  { "neo-tree.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
 }
